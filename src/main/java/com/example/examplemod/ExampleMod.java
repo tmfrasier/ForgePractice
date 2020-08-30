cb794f7374b00695a5b414c01e7eb6c680c6e0f4
Tim Frasier <5853580+tmfrasier@users.noreply.github.com>
2020-08-30 18:30:34 -0400

    add COBBLESTONE to logger under mod initization event

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -27,5 +27,6 @@ public class ExampleMod
     {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("COBBLESTONE BLOCK >> {}", Blocks.COBBLESTONE.getRegistryName());
     }
 }
